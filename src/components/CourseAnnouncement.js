75bd38f9dc21747fa33458ed4b197eeb88729be3
Dongsainity <mhan937@yahoo.com>
2017-07-16 17:29:00 +1200

    added NavBar and some subtle changes in UI

--- src/components/CourseAnnouncement.js
+++ src/components/CourseAnnouncement.js
@@ -4,6 +4,8 @@ import React from 'react';
 /* import data*/
 import announcementData from '../data/annoucncementData';
 import coursesData from '../data/coursesData';
+/*import images*/
+import circle from "../icons/circle.svg";
 import {NavLink} from 'react-router-dom';
 import "../scss/CourseAnnouncement.css";
 class CourseAnnouncement extends React.Component{
@@ -25,6 +27,13 @@ class CourseAnnouncement extends React.Component{
             return (
                 <div key={announcement.announcement_id}>
                     <NavLink to={`${this.props.match.url}/${announcement.announcement_id}`} >
+
+                            {announcement.read === false
+                                ?
+                                <img className="circle" src={circle} alt="circle"/>
+                                :
+                                <img className="style-holder" alt="circle"/>
+                            }
                             <span className="bar-title">{announcement.announcement_title}</span>
                             <span className="bar-person">  {announcement.person} </span>
                             <span className="bar-date">{announcement.announcement_date} </span>
